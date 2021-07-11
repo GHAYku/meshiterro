4dbf2e09dd3c7f6214a307fd33caf6b7c31b35d9
GHAYku <kusa07122@gmail.com>
2021-07-11 09:26:21 +0000

    [Finish]3-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key ='8388ec912474ee9bbc2301d61ba46a0963b66fde070eb4c3a8dc4d39a816c3dba9f4f409af067ff861b00db32e575fc244ac999db562dec792159b9ec1cb0e0f'
